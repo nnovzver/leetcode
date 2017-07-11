717b80b1af134d9800d7ff352231ca68746354d5
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-11 09:04:18 +0300

    Add Insertion Sort List problem

---
+++ insertion_sort_list.cpp
@@ -0,0 +1,17 @@
+// Insertion Sort List
+
+// Sort a linked list using insertion sort.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+    }
+};
