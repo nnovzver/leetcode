1a5b63652a8d16a1abc914091df6bddf6ce26865
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-13 13:25:09 +0300

    Add solution

--- insertion_sort_list.cpp
+++ insertion_sort_list.cpp
@@ -10,8 +10,81 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+#include "list_node.h"
+#include "catch.hpp"
+
+class Solution
+{
 public:
-    ListNode* insertionSortList(ListNode* head) {
+  void insertionSortListImpl(ListNode* head, bool& sorted)
+  {
+    if (head == nullptr or head->next == nullptr) {
+      sorted = true;
+      return;
     }
+
+    while (!sorted) {
+      if (head->next->val < head->val) {
+        std::swap(head->val, head->next->val);
+        return;
+      }
+      insertionSortListImpl(head->next, sorted);
+    }
+    return;
+  }
+
+  ListNode* insertionSortList(ListNode* head)
+  {
+    bool sorted = false;
+    while (!sorted) {
+      insertionSortListImpl(head, sorted);
+    }
+    return head;
+  }
 };
+
+TEST_CASE("insertionSortList work properly", "[insertionSortList]")
+{
+  Solution sol;
+
+  {
+    auto l1 = create_list();
+    REQUIRE(nullptr == sol.insertionSortList(l1.get()));
+  }
+  {
+    auto l1 = create_list(1);
+    auto l2 = create_list(1);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+  {
+    auto l1 = create_list(2, 1);
+    auto l2 = create_list(1, 2);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+  {
+    auto l1 = create_list(2, 3, 1);
+    auto l2 = create_list(1, 2, 3);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+  {
+    auto l1 = create_list(3, 2, 1);
+    auto l2 = create_list(1, 2, 3);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+  {
+    auto l1 = create_list(9, 8, 7, 6, 5, 4, 3, 2, 1);
+    auto l2 = create_list(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+  {
+    auto l1 = create_list(1, 3, 5, 4, 9, 8, 6, 7, 2);
+    auto l2 = create_list(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    sol.insertionSortList(l1.get());
+    REQUIRE(l2 == l1);
+  }
+}
