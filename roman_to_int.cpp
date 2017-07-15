81f7c1f730b110d738f7f004e49bab94210f00bc
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-15 14:55:37 +0300

    Add Roman to Integer problem

---
+++ roman_to_int.cpp
@@ -0,0 +1,15 @@
+// Roman to Integer
+
+// Given a roman numeral, convert it to an integer.
+// Input is guaranteed to be within the range from 1 to 3999.
+#include <string>
+
+using std::string;
+
+class Solution
+{
+public:
+  int romanToInt(string s)
+  {
+  }
+};
