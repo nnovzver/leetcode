2b087904c58d196a66eefe2b5ae2b2c8905ece7c
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-16 16:53:02 +0300

    Add Int to Roman problem

---
+++ int_to_roman.cpp
@@ -0,0 +1,26 @@
+// Integer to Roman
+
+// Given an integer, convert it to a roman numeral.
+// Input is guaranteed to be within the range from 1 to 3999.
+
+#include <string>
+#include "catch.hpp"
+
+using std::string;
+
+class Solution
+{
+public:
+  string intToRoman(int num)
+  {
+    num = 0;
+    return "i";
+  }
+};
+
+TEST_CASE("intToRoman work properly", "[intToRoman]")
+{
+  Solution sol;
+
+  REQUIRE(sol.intToRoman(1) == "i");
+}
