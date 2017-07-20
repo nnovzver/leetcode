4d7723df723aac368321313733c2ced51c113a2e
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-20 22:26:38 +0300

    Add Generate Parentheses problem

---
+++ generate_parentheses.cpp
@@ -0,0 +1,26 @@
+// Generate Parentheses
+
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// For example, given n = 3, a solution set is:
+
+// [
+// "((()))",
+// "(()())",
+// "(())()",
+// "()(())",
+// "()()()"
+// ]
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution
+{
+public:
+  vector<string> generateParenthesis(int n)
+  {
+  }
+};
