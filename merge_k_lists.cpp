02b590c0865894f872589389824e4810fe368299
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-24 11:16:41 +0300

    Add solution

--- merge_k_lists.cpp
+++ merge_k_lists.cpp
@@ -11,6 +11,9 @@
  * };
  */
 #include <vector>
+#include <algorithm>
+#include "list_node.h"
+#include "catch.hpp"
 
 using std::vector;
 
@@ -19,5 +22,77 @@ class Solution
 public:
   ListNode* mergeKLists(vector<ListNode*>& lists)
   {
+    ListNode* res = nullptr;
+    ListNode* tail = nullptr;
+
+    auto lists_begin = lists.begin();
+    auto lists_end = std::partition(lists_begin, lists.end(),
+      [](ListNode* v){return v != nullptr;});
+
+    // if no lists in vector
+    if (lists_end == lists_begin) return nullptr;
+
+    // init res and tail
+    auto min_head = std::min_element(lists_begin, lists_end,
+      [](ListNode* first, ListNode* second){return first->val < second->val;});
+    tail = res = *min_head;
+    *min_head = (*min_head)->next;
+
+    while (lists_begin != lists_end) {
+      auto min_head = std::min_element(lists_begin, lists_end,
+        [](ListNode* first, ListNode* second){return first->val < second->val;});
+      tail->next = *min_head;
+      tail = tail->next;
+      *min_head = (*min_head)->next;
+
+      lists_end = std::partition(lists_begin, lists_end,
+        [](ListNode* v){return v != nullptr;});
+      if (lists_end - lists_begin == 1) {
+        tail->next = *lists_begin;
+      }
+    }
+
+    return res;
   }
 };
+
+TEST_CASE("mergeKLists works properly", "[mergeKLists]")
+{
+  Solution sol;
+
+  {
+    vector<ListNode*> arg = {nullptr, nullptr, nullptr};
+    REQUIRE(nullptr == sol.mergeKLists(arg));
+  }
+
+  {
+    vector<ListNode*> arg = {create_list(1, 2, 3).release(), nullptr, nullptr};
+    ListNode* ret = sol.mergeKLists(arg);
+    auto res = create_list(1, 2, 3);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    vector<ListNode*> arg = {
+      create_list(1, 3, 5).release(),
+      create_list(2, 4, 6).release(),
+      nullptr};
+    auto res = create_list(1, 2, 3, 4, 5, 6);
+
+    ListNode* ret = sol.mergeKLists(arg);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+  {
+    vector<ListNode*> arg = {
+      create_list(1, 4, 7).release(),
+      create_list(2, 5, 8).release(),
+      create_list(3, 6, 9).release()};
+    auto res = create_list(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    ListNode* ret = sol.mergeKLists(arg);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+}
