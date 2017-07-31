03685d930926a661efbb3cfb869650072cbc5f35
Ruzanov Vladimir <nnovzver@gmail.com>
2017-07-31 16:52:33 +0300

    Add solution

--- solutions/remove_duplicates_from_array.cpp
+++ solutions/remove_duplicates_from_array.cpp
@@ -12,14 +12,101 @@
 // of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 
 #include <vector>
+#include <algorithm>
+#include "catch.hpp"
 
 using std::vector;
 
 class Solution
 {
 public:
-  int removeDuplicates(vector<int>& /*nums*/)
+  int removeDuplicates(vector<int>& nums)
   {
-    return 0;
+    return std::unique(nums.begin(), nums.end()) - nums.begin();
   }
 };
+
+class AlternativeSolution
+{
+public:
+  int removeDuplicates(vector<int>& nums)
+  {
+    auto i = nums.begin();
+
+    while (i != nums.end()) {
+      auto uniq = std::find_if_not(i, nums.end(), [&](int val){return val == *i;});
+      size_t dist = std::distance(i, uniq);
+      if (dist != 1) {
+        nums.erase(std::next(i), uniq);
+      }
+      i = std::next(i);
+    }
+    return std::distance(nums.begin(), nums.end());
+  }
+};
+
+TEST_CASE("removeDuplicates works properly")
+{
+  Solution sol;
+  AlternativeSolution altsol;
+
+  {
+    vector<int> v;
+    REQUIRE(0 == sol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1};
+    REQUIRE(1 == sol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1, 2, 2};
+    REQUIRE(2 == sol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1, 1, 2, 2, 3, 3, 3};
+    REQUIRE(3 == sol.removeDuplicates(v));
+  }
+
+  // alternative
+  {
+    vector<int> v;
+    REQUIRE(0 == altsol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1};
+    REQUIRE(1 == altsol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1, 2, 2};
+    REQUIRE(2 == altsol.removeDuplicates(v));
+  }
+  {
+    vector<int> v = {1, 1, 2, 2, 3, 3, 3};
+    REQUIRE(3 == altsol.removeDuplicates(v));
+  }
+}
+
+#ifdef SELFBENCH
+#include "benchmark/benchmark.h"
+static void BM_removeDuplicates(benchmark::State& state) {
+  Solution sol;
+  vector<int> v = {1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3};
+
+  while (state.KeepRunning()) {
+    int res = sol.removeDuplicates(v);
+    benchmark::DoNotOptimize(res);
+  }
+}
+BENCHMARK(BM_removeDuplicates);
+
+static void BM_removeDuplicates_alt(benchmark::State& state) {
+  AlternativeSolution sol;
+  vector<int> v = {1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3};
+
+  while (state.KeepRunning()) {
+    int res = sol.removeDuplicates(v);
+    benchmark::DoNotOptimize(res);
+  }
+}
+BENCHMARK(BM_removeDuplicates_alt);
+#endif // SELFBENCH
