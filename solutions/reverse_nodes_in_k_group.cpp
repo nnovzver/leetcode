419ec4de753487318a6d9358f3ef51db1571a1e0
Ruzanov Vladimir <nnovzver@gmail.com>
2017-08-02 14:12:19 +0300

    Add solution

--- solutions/reverse_nodes_in_k_group.cpp
+++ solutions/reverse_nodes_in_k_group.cpp
@@ -24,13 +24,121 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+#include <utility>
 #include "list_node.h"
+#include "catch.hpp"
 
 class Solution
 {
 public:
-  ListNode* reverseKGroup(ListNode* /*head*/, int /*k*/)
+  ListNode* reverseKGroup_iter(ListNode* cur, int k)
   {
-    return nullptr;
+    if (k == 0) {
+      return nullptr;
+    }
+    if (cur == nullptr) {
+      return nullptr;
+    }
+
+    ListNode* tail = reverseKGroup_iter(cur->next, k-1);
+    if (tail) {
+      cur->next = nullptr;
+      tail->next = cur;
+    }
+    return cur;
+  }
+
+  bool isListReversable(ListNode* head, int k, ListNode*& first, ListNode*& nextGroup)
+  {
+    int counter = k;
+    ListNode* i = head;
+    while (i) {
+      --counter;
+      if (counter == 0) break;
+      i = i->next;
+    }
+
+    if (counter == 0) {
+      first = i;
+      nextGroup = i->next;
+      return true;
+    }
+    else {
+      first = head;
+      nextGroup = nullptr;
+      return false;
+    }
+  }
+
+  ListNode* reverseKGroup(ListNode* head, int k)
+  {
+    ListNode* ret = head;
+    ListNode* nextGroup = nullptr;
+
+    bool isreversable = isListReversable(head, k, ret, nextGroup);
+    if (not isreversable) {
+      return head;
+    }
+
+    ListNode* tail;
+    do {
+      tail = reverseKGroup_iter(head, k);
+      head = nextGroup;
+    } while (isListReversable(head, k, tail->next, nextGroup));
+    return ret;
   }
 };
+
+TEST_CASE("reverseKGroup works properly")
+{
+  Solution sol;
+
+  {
+    ListNode* l = create_list(1, 2, 3).release();
+    auto res = create_list(1, 2, 3);
+    ListNode* ret = sol.reverseKGroup(l, 4);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    ListNode* l = create_list(1, 2, 3).release();
+    auto res = create_list(3, 2, 1);
+    ListNode* ret = sol.reverseKGroup(l, 3);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    ListNode* l = create_list(1, 2, 3).release();
+    auto res = create_list(2, 1, 3);
+    ListNode* ret = sol.reverseKGroup(l, 2);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    ListNode* l = create_list(1, 2, 3).release();
+    auto res = create_list(1, 2, 3);
+    ListNode* ret = sol.reverseKGroup(l, 1);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    ListNode* l = create_list(1, 2, 3, 4).release();
+    auto res = create_list(2, 1, 4, 3);
+    ListNode* ret = sol.reverseKGroup(l, 2);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+
+  {
+    ListNode* l = create_list(1, 2, 3, 4, 5, 6, 7, 8).release();
+    auto res = create_list(3, 2, 1, 6, 5, 4, 7, 8);
+    ListNode* ret = sol.reverseKGroup(l, 3);
+    REQUIRE(*res.get() == *ret);
+    delete_list(ret);
+  }
+}
