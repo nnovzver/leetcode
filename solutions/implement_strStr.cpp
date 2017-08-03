421ecbe1a81835a4222314f78377c81573f2094d
Ruzanov Vladimir <nnovzver@gmail.com>
2017-08-03 22:39:41 +0300

    Add Implement strStr problem

---
+++ solutions/implement_strStr.cpp
@@ -0,0 +1,17 @@
+// Implement strStr()
+
+// Returns the index of the first occurrence of needle in haystack,
+// or -1 if needle is not part of haystack.
+
+#include <string>
+
+using std::string;
+
+class Solution
+{
+public:
+  int strStr(string /*haystack*/, string /*needle*/)
+  {
+    return 0;
+  }
+};
