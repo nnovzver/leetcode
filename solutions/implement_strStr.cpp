fcfbaf0f4dc81e31b13f1f034a24a88ef9742416
Ruzanov Vladimir <nnovzver@gmail.com>
2017-08-04 10:53:45 +0300

    Add solution

--- solutions/implement_strStr.cpp
+++ solutions/implement_strStr.cpp
@@ -4,14 +4,26 @@
 // or -1 if needle is not part of haystack.
 
 #include <string>
+#include "catch.hpp"
 
 using std::string;
 
 class Solution
 {
 public:
-  int strStr(string /*haystack*/, string /*needle*/)
+  int strStr(string haystack, string needle)
   {
-    return 0;
+    auto n = haystack.find(needle);
+    return n != string::npos ? n : -1;
   }
 };
+
+TEST_CASE("strStr works properly")
+{
+  Solution sol;
+
+  REQUIRE(0 == sol.strStr("", ""));
+  REQUIRE(0 == sol.strStr("aba", "a"));
+  REQUIRE(1 == sol.strStr("aba", "ba"));
+  REQUIRE(-1 == sol.strStr("aba", "bar"));
+}
