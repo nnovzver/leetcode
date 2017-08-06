9f1baa06e8ce959bccd545dc4d7d7632575e07a6
Ruzanov Vladimir <nnovzver@gmail.com>
2017-08-06 22:06:43 +0300

    Add Divide Two Integers problem

---
+++ solutions/divide_two_integers.cpp
@@ -0,0 +1,14 @@
+// Divide Two Integers
+
+// Divide two integers without using multiplication, division and mod operator.
+
+// If it is overflow, return MAX_INT.
+
+class Solution
+{
+public:
+  int divide(int /*dividend*/, int /*divisor*/)
+  {
+    return 0;
+  }
+};
